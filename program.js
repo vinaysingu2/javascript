ebe2439cd5ef62f2434ffd60d7998714876c80e0
vinay <vinay.shire@gmail.com>
2012-08-30 02:33:21 -0700

    first commit

---
+++ program.js
@@ -0,0 +1,25 @@
+var myapp = {};
+myapp.flight = {
+"first-name": "Jerome",
+last: "Howard",
+name: "vinay",
+number: 815,
+departure:{
+ time: "1245",
+ place: "hyderabad"
+},
+arrival:{
+ time: "2:15",
+ place: "Delhi"
+}
+};
+//delete flight["first-name"];
+var name1;
+for (name1 in myapp.flight) {
+if(typeof myapp.flight[name1] == 'number')
+{	
+	document.writeln(name1 + ':' + myapp.flight[name1]);
+}
+
+}
+
